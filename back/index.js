308623510194ecbacac987ff02ca0037648e81c4
VMN13 <fx35@gmail.com>
2024-04-29 02:53:06 +0300

    initial

---
+++ back/index.js
@@ -0,0 +1,39 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6844732984:AAFk73dV2cBEgTR2BUjVbQwgGT9fXQrhqPY';
+const webAppUrl = 'https://evilchains.by/';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+})
+
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+
+   })
+
+
