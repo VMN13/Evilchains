c27e3e94c78721b5d4839bdb717a7d1602741de9
VMN13 <fx35@gmail.com>
2024-05-02 05:33:20 +0300

    initial

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import './Form.css';
 
 const Form = () => {
+    const [country, setCountry] = useState()
     return (
-        <div>
-Form
+       
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input 
+            className={'input'}
+             type="text"
+              placeholder={'Страна'}
+              />
+
+            <input
+             className={'input'}
+             type="text"
+              placeholder={'Улица'}
+              />
+              <select className={'select'}>
+                <option value={'oplata'}>Оплата картой</option>
+                <option value={'oplata'}>Наличными при получении</option>
+              </select>
         </div>
+       
     )
 };
 
