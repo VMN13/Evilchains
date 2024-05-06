9b6b6e57a55c567b22bdcbea5d982f304c6a46c7
VMN13 <fx35@gmail.com>
2024-05-06 05:33:11 +0300

    initial

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "./useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'ЦЕПЬ', price: 45, description: 'BROKEN HEART'},
+    {id: '2', title: 'ЦЕПЬ', price: 45, description: 'CUBAN'},
+    {id: '3', title: 'ЦЕПЬ', price: 45, description: 'BIG CROSS'},
+    {id: '4', title: 'БРАСЛЕТ', price: 25, description: 'BLINK'},
+    {id: '5', title: 'БРАСЛЕТ', price: 25, description: 'CUBAN'},
+    {id: '6', title: 'БРАСЛЕТ', price: 30, description: 'SLIM'},
+    {id: '7', title: 'Браслет', price: 15, description: 'CLASSIC'},
+    {id: '8', title: 'ОЧКИ', price: 35, description: 'RAZOR'},
 ]
 
 const getTotalPrice = (items = []) => {
